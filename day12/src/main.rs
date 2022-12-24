bf321e8178c6a2bd58949122e1a902faed8e479d
Josh Mullins <mullinsjoshuaj@gmail.com>
2022-12-24 13:45:41 -0500

    day 12 part 1

---
+++ day12/src/main.rs
@@ -0,0 +1,150 @@
+#[derive(Clone, Copy, Debug)]
+struct Point {
+    x: usize,
+    y: usize,
+    elevation: isize,
+}
+
+#[derive(Debug)]
+struct BoardPoint {
+    pos: Point,
+    visited: bool,
+}
+
+#[derive(Clone, Copy, Debug)]
+struct Path {
+    point: Point,
+    length: usize,
+}
+
+fn find_viable_neighbors(board: &mut Vec<Vec<BoardPoint>>, point: &Point) -> Vec<Point> {
+    let mut neighbors = Vec::new();
+    // Check around point to see if any are viable
+    if point.x > 0 {
+        let left = &board[point.y][point.x - 1];
+        if !left.visited && (left.pos.elevation == point.elevation + 1 || left.pos.elevation <= point.elevation) {
+            neighbors.push(left.pos);
+        }
+    }
+    if point.x < board[0].len() - 1 {
+        let right = &board[point.y][point.x + 1];
+        if !right.visited && (right.pos.elevation == point.elevation + 1 || right.pos.elevation <= point.elevation) {
+            neighbors.push(right.pos);
+        }
+    }
+    if point.y > 0 {
+        let up = &board[point.y - 1][point.x];
+        if !up.visited && (up.pos.elevation == point.elevation + 1 || up.pos.elevation <= point.elevation) {
+            neighbors.push(up.pos);
+        }
+    }
+    if point.y < board.len() - 1 {
+        let down = &board[point.y + 1][point.x];
+        if !down.visited && (down.pos.elevation == point.elevation + 1 || down.pos.elevation <= point.elevation) {
+            neighbors.push(down.pos);
+        }
+    }
+
+    for neighbor in neighbors.iter() {
+        board[neighbor.y][neighbor.x].visited = true;
+    }
+
+    neighbors
+}
+
+fn main() {
+    let elevation_order = "SabcdefghijklmnopqrstuvwxyzE";
+    // let input = include_str!("example.txt");
+    let input = include_str!("input.txt");
+    // get a grid of items from input as usize based on index in elevation order
+    let grid = input
+        .lines()
+        .map(|line| {
+            line.chars()
+                .map(|c| elevation_order.find(c).unwrap())
+                .collect::<Vec<usize>>()
+        })
+        .collect::<Vec<Vec<usize>>>();
+
+    // map grid into BoardPoint
+    let mut board = grid
+        .iter()
+        .enumerate()
+        .map(|(row, row_items)| {
+            row_items
+                .iter()
+                .enumerate()
+                .map(|(col, item)| BoardPoint {
+                    pos: Point {
+                        x: col,
+                        y: row,
+                        elevation: *item as isize,
+                    },
+                    visited: false,
+                })
+                .collect::<Vec<BoardPoint>>()
+        })
+        .collect::<Vec<Vec<BoardPoint>>>();
+
+    let start = board
+        .iter()
+        .flat_map(|row| row.iter())
+        .find(|point| point.pos.elevation == 0)
+        .unwrap()
+        .pos;
+
+    let max_elevation = board
+        .iter()
+        .flat_map(|row| row.iter())
+        .map(|point| point.pos.elevation)
+        .max()
+        .unwrap();
+
+    // let mut queue: Vec<Point> = vec![start];
+    let mut queue: Vec<Path> = vec![Path {
+        point: start,
+        length: 0,
+    }];
+    board[start.y][start.x].visited = true;
+
+    let mut final_path: Path = Path {
+        point: start,
+        length: 0,
+    };
+
+    while !queue.is_empty() {
+        let path = queue[0];
+        final_path = path;
+        queue.remove(0);
+        if path.point.elevation == max_elevation {
+            println!("Found end point: {:?}", path.point);
+            // part1 = path.length;
+            final_path = path;
+            break;
+        }
+        let neighbors = find_viable_neighbors(&mut board, &path.point);
+        for neighbor in neighbors.iter() {
+            queue.push(Path {
+                point: *neighbor,
+                length: path.length + 1,
+            });
+        }
+    }
+
+    // // visualize the grid
+    // for y in 0..board.len() {
+    //     for x in 0..board[0].len() {
+    //         let point = &board[y][x];
+    //         if point.pos.x == final_path.point.x && point.pos.y == final_path.point.y {
+    //             print!("[=]");
+    //         } else if point.visited {
+    //             print!("[{}]", elevation_order.chars().nth(point.pos.elevation as usize).unwrap());
+    //         } else {
+    //             print!(" {} ", elevation_order.chars().nth(point.pos.elevation as usize).unwrap());
+    //         }
+    //     }
+    //     println!();
+    // }
+
+    println!("Part 1: {}", final_path.length);
+}
