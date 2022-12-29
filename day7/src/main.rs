dcb75b7799564a20e8ce48b14d21aa545c52574a
Josh Mullins <mullinsjoshuaj@gmail.com>
2022-12-28 22:39:17 -0500

    day 7

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -2,7 +2,6 @@ use std::collections::HashMap;
 
 #[derive(Debug, Clone)]
 struct Item {
-    path: String,
     parent_path: String,
     is_dir: bool,
     size: u64,
@@ -21,7 +20,6 @@ fn handle_cd(name: &str, mut cur_path: String, items: &mut HashMap<String, Item>
     let path = cur_path.to_string() + name + "/";
     if !items.contains_key(&path) {
         let item = Item {
-            path: path.clone(),
             parent_path: cur_path.to_string(),
             is_dir: true,
             size: 0,
@@ -36,7 +34,6 @@ fn parse_terminal(input: &str) -> HashMap<String, Item> {
     items.insert(
         "/".to_string(),
         Item {
-            path: "/".to_string(),
             parent_path: "/".to_string(),
             is_dir: true,
             size: 0,
@@ -49,38 +46,27 @@ fn parse_terminal(input: &str) -> HashMap<String, Item> {
             if parts[1] == "cd" {
                 cur_path = handle_cd(parts[2], cur_path, &mut items);
             }
-            // else if parts[1] == "ls" {
-            //     println!();
-            // }
-        }
-
-        else if parts[0] == "dir" {
+        } else if parts[0] == "dir" {
             let name = parts[1];
             let path = cur_path.to_string() + name;
             if !items.contains_key(&path) {
                 let item = Item {
-                    path: path.clone(),
                     parent_path: cur_path.to_string(),
                     is_dir: true,
                     size: 0,
                 };
                 items.insert(path.clone(), item);
             }
-        }
-
-        else {
+        } else {
             let size = parts[0].parse::<u64>().unwrap();
             let name = parts[1];
             let path = cur_path.to_string() + name;
-            // if !items.contains_key(&path) {
             let item = Item {
-                path: path.clone(),
                 parent_path: cur_path.to_string(),
                 is_dir: false,
                 size,
             };
             items.insert(path.clone(), item);
-            // }
         }
     }
     items
@@ -90,23 +76,22 @@ fn propagate_size(item: &Item, items: &mut HashMap<String, Item>) {
     let mut queue = vec![item.parent_path.clone()];
     while !queue.is_empty() {
         let path = queue.pop().unwrap();
-        if path == "/" {
-            break;
-        }
         let parent = items.get_mut(&path).unwrap();
-        // let parent = items.get_mut(path).unwrap();
-        // let item = &items[path];
         if parent.is_dir {
             parent.size += item.size;
+            if path == "/" {
+                break;
+            }
             queue.push(parent.parent_path.clone());
         }
     }
 }
 
 fn main() {
-    let input = include_str!("example.txt");
+    // let input = include_str!("example.txt");
+    let input = include_str!("input.txt");
     let mut items = parse_terminal(input);
-    for item in items.clone().values_mut() { //.clone().values_mut()
+    for item in items.clone().values_mut() {
         propagate_size(item, &mut items);
     }
 
@@ -117,5 +102,23 @@ fn main() {
 
     // sum size of all files under 100k
     let sum = items_under_100k.iter().map(|item| item.size).sum::<u64>();
-    println!("{:#?}", sum);
+
+    let fs_size = 70_000_000;
+    let space_needed = 30_000_000;
+    let space_used = items["/"].size;
+    let space_left = fs_size - space_used;
+    let space_needed = space_needed - space_left;
+
+    // find smallest dir that is larger than space_needed
+    let smallest_delete_dir = items
+        .values()
+        .filter(|item| item.is_dir && item.size > space_needed)
+        .min_by_key(|item| item.size)
+        .unwrap();
+
+    println!("Part 1: {}", sum);
+    println!("Part 2: {}", smallest_delete_dir.size);
+
+    assert_eq!(sum, 2_061_777);
+    assert_eq!(smallest_delete_dir.size, 4_473_403);
 }
